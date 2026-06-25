9a0b224c39cabad1fb0a591317422bda0c034f93
policyplanner1 <it@policyplanner.com>
2026-06-26 01:36:16 +0530

    step backend removed

--- src/modules/step_counter/context/step_counter_backend/routes/progressRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ProgressController = require("../controllers/progressController");
-const auth = require("../../../common/middlewares/auth");
-
-// progress
-router.get("/calendar", auth, ProgressController.getCalendar);
-
-module.exports = router;
