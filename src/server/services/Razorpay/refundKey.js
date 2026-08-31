63ac48764e32e5548f1d1f1aeb121b2ef5f07fc4
policyplanner1 <it@policyplanner.com>
2026-08-31 11:32:06 +0530

    server files removed and metro added

--- src/server/services/Razorpay/refundKey.js
+++
@@ -1,8 +0,0 @@
-function makeRefundKey({ orderId, shipmentId, paymentId, refundKey }) {
-  if (refundKey) return refundKey;
-  if (shipmentId) return `shipment_${shipmentId}_rto_refund`;
-  if (paymentId) return `payment_${paymentId}_duplicate_refund`;
-  return `order_${orderId}_cancel_refund`;
-}
-
-module.exports = { makeRefundKey };
